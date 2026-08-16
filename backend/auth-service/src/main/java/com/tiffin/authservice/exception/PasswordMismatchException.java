72904eab142250df83571d72ac8421fea1d4e636
ahsi58 <patilisha2002@gmail.com>
2026-08-16 18:15:01 +0530

    Removed unwanted files

--- backend/auth-service/src/main/java/com/tiffin/authservice/exception/PasswordMismatchException.java
+++
@@ -1,9 +0,0 @@
-package com.tiffin.authservice.exception;
-
-public class PasswordMismatchException extends RuntimeException {
-
-    public PasswordMismatchException(String message) {
-        super(message);
-    }
-
-}
--- backend/auth-service/src/main/java/com/tiffin/authservice/exception/UserProfileCreationException.java
+++
@@ -1,9 +0,0 @@
-package com.tiffin.authservice.exception;
-
-public class UserProfileCreationException extends RuntimeException {
-
-    public UserProfileCreationException(String message) {
-        super(message);
-    }
-
-}
